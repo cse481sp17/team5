7ffccc3b18820edb1fb99b56d57b221a5771737f
Mahir Kothary <m.kothary95@gmail.com>
2017-05-21 21:04:01 -0700

    update perception module

--- barbot/src/segment_objects.cpp
+++ barbot/src/segment_objects.cpp
@@ -1,7 +1,10 @@
 #include "perception/segmentation.h"
 #include "ros/ros.h"
 #include "barbot/segment_objects.h"
+
 #include <string>
+#include <math.h>
+#include <std_msgs/Float64.h>
 
 #include "sensor_msgs/PointCloud2.h"
 #include "visualization_msgs/Marker.h"
@@ -19,7 +22,6 @@
 #include "pcl/common/common.h"
 #include "pcl/segmentation/extract_clusters.h"
 
-
 typedef pcl::PointXYZRGB PointC;
 typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloudC;
 
@@ -28,11 +30,17 @@ typedef barbot::MoveToPerception MpServ;
 namespace barbot {
     SegmentObjects::SegmentObjects(const ros::Publisher& marker_pub)
         : marker_pub_(marker_pub), camera_pointCloud_(new PointCloudC()){}
+
+    double SegmentObjects::calc_minkowski_distance(geometry_msgs::Position start, geometry_msgs::Position end) {
+        double aggreg = 0.0;
+        aggreg += pow(abs(start.x-end.x), 2);
+        aggreg += pow(abs(start.y-end.y), 2);
+        aggreg += pow(abs(start.z-end.z), 2);
+        return pow(aggreg, 1.0/2.0);
+    }
+
     bool SegmentObjects::ServiceCallback(MpServ::Request  &req, MpServ::Response &res) {
         ROS_INFO("Service call received of type %s", req.perception.c_str());
-        // PointCloudC::Ptr cloudIn(new PointCloudC());
-        // cloudIn->is_dense = false;
-        // pcl::fromROSMsg(camera_pointCloud_, *cloudIn);
         PointCloudC::Ptr cropped_cloud(new PointCloudC());
         double min_x, min_y, min_z, max_x, max_y, max_z;
         ros::param::param("crop_min_x", min_x, 0.3);
@@ -62,49 +70,59 @@ namespace barbot {
         extract.setInputCloud(cloud);
         extract.setIndices(table_inliers);
         extract.filter(*segmented_cloud);
-
+        geometry_msgs::Position finalPosition;
         // // SEND MARKER FOR TABLE
-        visualization_msgs::Marker table_marker;
-        table_marker.ns = "table";
-        table_marker.header.frame_id = "base_link";
-        table_marker.type = visualization_msgs::Marker::CUBE;
-        perception::GetAxisAlignedBoundingBox(segmented_cloud, &table_marker.pose, &table_marker.scale);
-        table_marker.color.r = 1;
-        table_marker.color.a = 0.8;
-        marker_pub_.publish(table_marker);
+        if(req.perception.c_str() == req.TABLE.c_str()) {
+            visualization_msgs::Marker table_marker;
+            table_marker.ns = "table";
+            table_marker.header.frame_id = "base_link";
+            table_marker.type = visualization_msgs::Marker::CUBE;
+            perception::GetAxisAlignedBoundingBox(segmented_cloud, &table_marker.pose, &table_marker.scale);
+            table_marker.color.r = 1;
+            table_marker.color.a = 0.8;
+            marker_pub_.publish(table_marker);
+            ROS_INFO('Generated marker for table');
+            finalPosition.x = table.pose.position.x;
+            finalPosition.y = table.pose.position.y;
+            finalPosition.z = table.pose.position.z;
+        } else if (req.perception.c_str() == req.CUP.c_str()) {
+            std::vector<pcl::PointIndices> object_indices;
+            std::vector<visualization_msgs::Marker> marker_vector;
+            perception::SegmentSurfaceObjects(cloud, table_inliers, &object_indices);
+            ROS_INFO('Generated marker for cups');
+            for (size_t i = 0; i < object_indices.size(); ++i) {
+            // Reify indices into a point cloud of the object.
+                pcl::PointIndices::Ptr indice(new pcl::PointIndices);
+                *indice = object_indices[i];
+                PointCloudC::Ptr object_cloud(new PointCloudC());
 
-        std::vector<pcl::PointIndices> object_indices;
-        std::vector<visualization_msgs::Marker> marker_vector;
-        perception::SegmentSurfaceObjects(cloud, table_inliers, &object_indices);
-
-        for (size_t i = 0; i < object_indices.size(); ++i) {
-        // Reify indices into a point cloud of the object.
-            pcl::PointIndices::Ptr indice(new pcl::PointIndices);
-            *indice = object_indices[i];
-            PointCloudC::Ptr object_cloud(new PointCloudC());
-
-            pcl::ExtractIndices<PointC> extract3;
-            extract3.setInputCloud(cloud);
-            extract3.setIndices(indice);
-            extract3.setNegative(false);
-            extract3.filter(*object_cloud);
-            // Publish a bounding box around it.
-            visualization_msgs::Marker object_marker;
-            object_marker.ns = "objects";
-            object_marker.id = i;
-            object_marker.header.frame_id = "base_link";
-            object_marker.type = visualization_msgs::Marker::CUBE;
-            perception::GetAxisAlignedBoundingBox(object_cloud, &object_marker.pose,
-                                        &object_marker.scale);
-            object_marker.color.b = 1;
-            object_marker.color.g = 0.7;
-            object_marker.color.a = 0.5;
-            marker_vector.push_back(object_marker);
-            marker_pub_.publish(object_marker);
+                pcl::ExtractIndices<PointC> extract3;
+                extract3.setInputCloud(cloud);
+                extract3.setIndices(indice);
+                extract3.setNegative(false);
+                extract3.filter(*object_cloud);
+                // Publish a bounding box around it.
+                visualization_msgs::Marker object_marker;
+                object_marker.ns = "objects";
+                object_marker.id = i;
+                object_marker.header.frame_id = "base_link";
+                object_marker.type = visualization_msgs::Marker::CUBE;
+                perception::GetAxisAlignedBoundingBox(object_cloud, &object_marker.pose,
+                                            &object_marker.scale);
+                object_marker.color.b = 1;
+                object_marker.color.g = 0.7;
+                object_marker.color.a = 0.5;
+                marker_vector.push_back(object_marker);
+                marker_pub_.publish(object_marker);
+                finalPosition.x = object_marker.pose.position.x;
+                finalPosition.y = object_marker.pose.position.y;
+                finalPosition.z = object_marker.pose.position.z;
+            }
         }
-        res.x = 9;
-        res.y = 9;
-        res.z = 1;
+
+        res.x = finalPosition.x;
+        res.y = finalPosition.y;
+        res.z = finalPosition.z;
         res.item = req.perception;
         ROS_INFO("sending back response: %s", res.item.c_str());
         return true;
